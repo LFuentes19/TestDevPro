91547e938eb3c8f748ff29b739c020c76dec2a77
LFuentes19 <167472627+LFuentes19@users.noreply.github.com>
2024-04-18 17:00:47 -0300

    Update TestLogger.cs

--- TestLogger.cs
+++ TestLogger.cs
@@ -15,13 +15,13 @@ public class LoggerTests
         // Test ERROR message
         Logger.LogMessage("application.log", "Database connection failed", "ERROR");
 		
-    		//Test Multiple Message Logging same file
-    		Logger.LogMessage("application.log", "User logged in", "INFO")
-    		Logger.LogMessage("application.log", "Failed login attempt", "WARNING")
-    		Logger.LogMessage("application.log", "Database connection failed", "ERROR")
-    			
-    		//Test Multiple Message Logging different file
-    		Logger.LogMessage("errors.log", "Critical error occurred", "CRITICAL")
-    		Logger.LogMessage("activity.log", "User performed action A", "INFO")
+	//Test Multiple Message Logging same file
+	Logger.LogMessage("application.log", "User logged in", "INFO")
+	Logger.LogMessage("application.log", "Failed login attempt", "WARNING")
+	Logger.LogMessage("application.log", "Database connection failed", "ERROR")
+		
+	//Test Multiple Message Logging different file
+	Logger.LogMessage("errors.log", "Critical error occurred", "CRITICAL")
+	Logger.LogMessage("activity.log", "User performed action A", "INFO")
     }
 }
